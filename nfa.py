41aa1a5aa5c722ef0ec5edef451801a88cb225be
Hazem Fahmi <hzmmohamed@gmail.com>
2021-05-09 22:27:35 +0200

    added thompson's rules

--- nfa.py
+++ nfa.py
@@ -35,13 +35,16 @@ class nfa:
         else:
             self.terminating_states.append(state)
 
-    def add_transition(self, from_state, to_state, input):
-        if not((from_state in self.states) and (to_state in self.states)):
+    def add_transition(self, from_state, to_states, input):
+        if not((from_state in self.states) and \
+            (set(to_states).issubset(set(self.states)))):
             raise Exception('Make sure all specified states exist in the NFA.')
         elif input not in alphabet:
             raise Exception('Invalid input parameter.')
+        elif input in self.transitions[from_state].keys():
+            self.transitions[from_state][input].extend(to_states)
         else:
-            self.transitions[from_state][input] = to_state
+            self.transitions[from_state][input] = to_states
 
 
     def to_numeric_states(self, starting_index):
@@ -53,8 +56,8 @@ class nfa:
             starting_index +=1
 
         for state, index in new_indices.items():
-            for input, to_state in self.transitions[state].items():
-                new_nfa.add_transition(index, new_indices[to_state], input)
+            for input, to_states in self.transitions[state].items():
+                new_nfa.add_transition(index, [new_indices[to_state] for to_state in to_states], input)
 
         
         new_nfa.set_starting(new_indices[self.starting_state])
@@ -74,9 +77,9 @@ class nfa:
                 s['isTerminatingState'] = True
             else:
                 s['isTerminatingState'] = False
-            for input, to_state in nfa.transitions[state].items():
-                s[input] = 'S'+str(to_state)
+            for input, to_states in nfa.transitions[state].items():
+                s[input] = ['S'+str(to_state) for to_state in to_states]
             nfa_json['S'+str(state)] = s
-        nfa_json_str =  json.dumps(nfa_json, indent=3)
+        nfa_json_str =  json.dumps(nfa_json, indent=3,)
         return nfa_json_str
 
--- thompson_rules.py
+++ thompson_rules.py
@@ -23,13 +23,63 @@ def concat_rule(nfa1, nfa2):
     concat_nfa.set_terminating(nfa2_.terminating_states[0])
 
     # add epsilon transtion between
-    concat_nfa.add_transition(nfa1_.terminating_states[0], nfa2_.starting_state, '\u03B5')
+    concat_nfa.add_transition(nfa1_.terminating_states[0], [nfa2_.starting_state], '\u03B5')
 
     return concat_nfa
 
 
 
+def or_rule(nfa1, nfa2):
+    nfa1_, n1 = nfa1.to_numeric_states(1)
+    nfa2_, n2 = nfa2.to_numeric_states(n1)
+
+    or_nfa = nfa()
+    
+    for state in nfa1_.states + nfa2_.states:
+        or_nfa.add_state(state)
+
+    # add transitions from NFAs 1 and 2
+    for from_state, trns in nfa1_.transitions.items():
+        for input, to_states in trns.items():
+            or_nfa.add_transition(from_state, to_states, input)
+    
+    for from_state, trns in nfa2_.transitions.items():
+        for input, to_states in trns.items():
+            or_nfa.add_transition(from_state, to_states, input)
 
 
+    # add start and final states and corresponding transitions
+    or_nfa.add_state(0)
+    or_nfa.set_starting(0)
+    or_nfa.add_transition(0, [1], '\u03B5')
+    or_nfa.add_transition(0, [n1], '\u03B5')
+
+    or_nfa.add_state(n2)
+    or_nfa.set_terminating(n2)
+    or_nfa.add_transition(n1-1, [n2], '\u03B5')
+    or_nfa.add_transition(n2-1, [n2], '\u03B5')
+
+    return or_nfa
+
+
+def star_rule(nfa1):
+    nfa1_, n = nfa1.to_numeric_states(1)
+
+    star_nfa = nfa()
+
+    for state in nfa1_.states:
+        star_nfa.add_state(state)
+
+    for from_state, trns in nfa1_.transitions.items():
+        for input, to_state in trns.items():
+            star_nfa.add_transition(from_state, to_state, input)
+
+    star_nfa.add_state(0)
+    star_nfa.add_state(n)
+
+    star_nfa.add_transition(0, [1], '\u03B5')
+    star_nfa.add_transition(n-1, [n], '\u03B5')
+    star_nfa.add_transition(0, [n], '\u03B5')
+    star_nfa.add_transition(n-1, [1], '\u03B5')
 
 
